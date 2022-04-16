6a6372cc9be437e3ce6a9239bb262dfa923f0f5f
Chinmay Tiwari <chinmaytanmay@outlook.com>
2022-04-16 23:53:20 +0000

    added more block types

--- block.py
+++ block.py
@@ -1,6 +1,19 @@
+types = {
+  "stone": (153, 151, 142),
+  "dirt": (74, 42, 37)
+}
+
 class block:
   def __init__(self,x,y,color):
     self.x = x
     self.y = y
     self.color = color
     self.prevcolor = color
+  def changecolor(self,type):
+    try: 
+      self.color = types[type]
+      self.prevcolor = self.color
+      print("Color changed")
+    except KeyError:
+      return 0
+  
--- main.py
+++ main.py
@@ -9,7 +9,7 @@ pygame.mixer.init()
 #pygame.mixer.music.play(-1)
 (width, height) = (480, 360)
 background_colour = (0,0,255)
-pygame.display.set_caption('block game')
+pygame.display.set_caption('MC RIPOFF Alpha 0.1')
 clock = pygame.time.Clock()
 screen = pygame.display.set_mode((width, height))
 pygame.display.flip()
@@ -19,7 +19,7 @@ scx = 0
 scy = 0
 """Create blocks"""
 blocks = []
-amount = 300
+amount = 1000
 x = 0
 y = 0
 zoom = 1
@@ -37,14 +37,14 @@ for i in range(amount):
 img = pygame.image.load('mc.png')
 imgx = 50
 imgy = 230
-
+currentblockcolor = "stone"
 while running:
   #print("Zoom: "+str(zoom),end='\r')
   pygame.display.flip()
   screen.fill(background_colour)
   (mouseX, mouseY) = pygame.mouse.get_pos()
   for block in blocks:  
-    if ((((block.x+scx)-mouseX)*((block.x+scx)-mouseX))+((((block.y+scy)-mouseY)*((block.y+scy)-mouseY)))) < 15:
+    if math.sqrt((((block.x+scx)-mouseX)*((block.x+scx)-mouseX))+((((block.y+scy)-mouseY)*((block.y+scy)-mouseY)))) < 5:
       block.color = (255,0,0)
     else:
       block.color = block.prevcolor
@@ -55,7 +55,11 @@ while running:
   text = "FPS: " + str(clock.get_fps()) #get fps
   textsurface = myfont.render(text, False, (0, 20, 0))
   screen.blit(textsurface, (0, 0))
-  screen.blit(img, (imgx, imgy))
+  text = "BLOCK: " + str(currentblockcolor) #get fps
+  textsurface = myfont.render(text, False, (0, 20, 0))
+  screen.blit(textsurface, (0, 30))
+  
+  #screen.blit(img, (imgx, imgy))
   if keys[pygame.K_UP]:
       scy += 10
   if keys[pygame.K_DOWN]:
@@ -64,8 +68,18 @@ while running:
       scx += 10
   if keys[pygame.K_RIGHT]:
       scx -= 10
+  """diffent block types"""
+  if keys[pygame.K_1]:
+    currentblockcolor = "stone"
+  if keys[pygame.K_2]:
+    currentblockcolor = "dirt"
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       running = False
+    if event.type == pygame.MOUSEBUTTONUP:
+      for block in blocks:  
+        if math.sqrt((((block.x+scx)-mouseX)*((block.x+scx)-mouseX))+((((block.y+scy)-mouseY)*((block.y+scy)-mouseY)))) < 10:
+          block.changecolor(currentblockcolor)
+      
     
 
